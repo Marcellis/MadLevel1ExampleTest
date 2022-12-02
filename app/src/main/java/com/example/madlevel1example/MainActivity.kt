999c85874c24dea6383d1c1806ee376c3eb15c4f
marcomarcellis <m.m.c.m.marcellis@hva.nl>
2022-12-02 15:16:11 +0100

    second commit

--- app/src/main/java/com/example/madlevel1example/MainActivity.kt
+++ app/src/main/java/com/example/madlevel1example/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.madlevel1example
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
@@ -9,17 +11,15 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Send
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.madlevel1example.ui.theme.MadLevel1ExampleTheme
-import androidx.compose.material.OutlinedTextField as OutlinedTextField
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -53,8 +53,6 @@ fun GuessAnimalScreen() {
 @Composable
 fun ScreenContent() {
 
-    // Local variable to hold and save the value of the text entered by the user.
-    var answerText by remember { mutableStateOf(String()) }
 
     Column(
         Modifier
@@ -72,30 +70,52 @@ fun ScreenContent() {
             painter = painterResource(id = R.drawable.giraffe),
             contentDescription = "", // decorative element
             modifier = Modifier
-                .padding (all = 16.dp)
+                .padding(all = 16.dp)
                 .width(250.dp)
                 .height(250.dp)
         )
         Row() {
-            OutlinedTextField(
-                value = answerText,
-                // Below line is used to add placeholder ("hint") for our text field.
-                placeholder = { Text(text = stringResource(id = R.string.animal_question)) },
-                onValueChange = {
-                                answerText = it
-                },
-                label = { Text(stringResource(R.string.answer_label)) }
-            )
-            Button(
-                modifier = Modifier.padding(start = 24.dp, top = 12.dp),
-                onClick = {
-                    // TODO To be completed
-                }
-            ) {
-                Icon(Icons.Filled.Send, "Process user input")
-            }
+            InvoerRij()
+
         }
     }
+}
+
+@Composable
+private fun InvoerRij() {
+    // Local variable to hold and save the value of the text entered by the user.
+    var answerText by remember { mutableStateOf(String()) }
+    // Context is needed for displaying a Toast message.
+
+    OutlinedTextField(
+        value = answerText,
+        // Below line is used to add placeholder ("hint") for our text field.
+        placeholder = { Text(text = stringResource(id = R.string.animal_question)) },
+        onValueChange = {
+            answerText = it
+        },
+        label = { Text(stringResource(R.string.answer_label)) }
+    )
+
+    val context = LocalContext.current
+
+    Button(
+        modifier = Modifier.padding(start = 24.dp, top = 12.dp),
+        onClick = { VerifyAnswerFab(context, answerText) }) {
+        Icon(Icons.Filled.Send, "Process user input")
+
+    }
+
+
+}
+
+fun VerifyAnswerFab(context: Context, answerText: String) {
+    val giraffe = "GIRAFFE"
+    val isCorrect = " is correct"
+    val isIncorrect = " is incorrect"
+    var txt = "\"" + answerText + "\""
+    if (answerText.uppercase().equals(giraffe)) txt += isCorrect else txt += isIncorrect
+    Toast.makeText(context, txt, Toast.LENGTH_SHORT).show()
 }
 
 @Preview(showBackground = true)
